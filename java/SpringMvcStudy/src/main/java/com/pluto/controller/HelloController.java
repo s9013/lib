f347791ab784d3e36962d7509937ba1f3ad96652
scorpion <528847630@qq.com>
2015-07-28 15:05:32 +0800

    spring view resovle

--- java/SpringMvcStudy/src/main/java/com/pluto/controller/HelloController.java
+++ java/SpringMvcStudy/src/main/java/com/pluto/controller/HelloController.java
@@ -1,11 +1,15 @@
 package com.pluto.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.pluto.service.iface.HelloService;
 
@@ -14,7 +18,7 @@ import com.pluto.service.iface.HelloService;
  * @author       Jay
  * @time         2015Äê7ÔÂ20ÈÕ
  */
-@RestController
+@Controller
 @RequestMapping(value="/api")
 public class HelloController {
 
@@ -24,11 +28,20 @@ public class HelloController {
 	HelloService helloService; 
 	
 	@RequestMapping(value="hello")
+	@ResponseBody
 	public String hello(@RequestParam(value="name") String name){
 		log.info("hello");
 		
 		String str = helloService.hello(name);
 		return "hello:" + str;
+	}
+	
+	@RequestMapping(value="hellojsp")
+	public String helloJsp(@RequestParam(value="name") String name,Model model){
+		log.info("hellojsp");
+		String str = helloService.hello(name);
+		model.addAttribute("hello", str);
+		return "hello";
 	}
 	
 }
