fab654a3e0159b5c202d88063823b106f4461cd8
scorpion <528847630@qq.com>
2015-08-13 18:13:15 +0800

    jpa change

--- java/JPA/src/test/java/com/jpa/AppTest.java
+++ java/JPA/src/test/java/com/jpa/AppTest.java
@@ -4,81 +4,62 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.jpa.entity.User;
 
 public class AppTest {
 
+	EntityManagerFactory factory;
+	EntityManager em;
+	
+	@Before
+	public void init(){
+		// 获取实体管理工厂
+		factory = Persistence.createEntityManagerFactory("mysqlJPA");
+		// 获取实体管理
+		em = factory.createEntityManager();
+		// 开启事物
+		em.getTransaction().begin();
+	}
+	
+	@After
+	public void close(){
+		// 提交事物
+		em.getTransaction().commit();
+		// 关闭实体管理
+		em.close();
+		// 关闭实体管理工厂
+		factory.close();
+	}
+	
 	@Test
 	public void save() {
-
-		EntityManagerFactory factory = Persistence
-				.createEntityManagerFactory("mysqlJPA");
-
-		EntityManager em = factory.createEntityManager();
-
-		em.getTransaction().begin();
-
+		// 保存
 		em.persist(new User("scorpion", 21));
-
-		em.getTransaction().commit();
-
-		em.close();
-
-		factory.close();
 	}
 
 	@Test
 	public void find() {
-
-		EntityManagerFactory factory = Persistence
-				.createEntityManagerFactory("mysqlJPA");
-
-		EntityManager em = factory.createEntityManager();
-
-		em.getTransaction().begin();
-
+		// 查找
 		User user = em.find(User.class, 1);
-
 		System.out.println(user);
-
-		em.close();
-
-		factory.close();
 	}
 
 	@Test
 	public void update() {
-
-		EntityManagerFactory factory = Persistence
-				.createEntityManagerFactory("mysqlJPA");
-
-		EntityManager em = factory.createEntityManager();
-
-		em.getTransaction().begin();
-		User user = em.find(User.class, 9);
-		user.setAge(100);
-		em.getTransaction().commit();
-		em.close();
-
-		factory.close();
+		// 更新
+		User user = em.find(User.class, 1);
+		user.setAge(100);// 直接更改
 	}
 
 	@Test
 	public void delete() {
-
-		EntityManagerFactory factory = Persistence
-				.createEntityManagerFactory("mysqlJPA");
-
-		EntityManager em = factory.createEntityManager();
-
-		em.getTransaction().begin();
-		User user = em.find(User.class, 9);
+		// 查找
+		User user = em.find(User.class, 12);
+		// 删除
 		em.remove(user);
-		em.getTransaction().commit();
-		em.close();
-
-		factory.close();
 	}
 }
